9659950f67778fb377badb3fdfd8f4d6548b096a
Subhani6697 <shaiksubhani6697@gmail.com>
2024-11-06 21:41:41 -0600

    pics

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bookshelvesRouter = require('./routes/bookshelves');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./pick/grid');
 var app = express();
 
 // view engine setup
@@ -24,7 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bookshelves', bookshelvesRouter);
 app.use('/grid',gridRouter);
-
+app.use('/pick',pickRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
