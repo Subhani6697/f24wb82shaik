e4eaad769ffd3d1594aa7598bf39044106d52098
Subhani6697 <shaiksubhani6697@gmail.com>
2024-11-06 21:42:39 -0600

    pics

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bookshelvesRouter = require('./routes/bookshelves');
 var gridRouter = require('./routes/grid');
-var pickRouter = require('./pick/grid');
+var pickRouter = require('./routes/pick');
 var app = express();
 
 // view engine setup
